d4a28b1f3deb1e812354bb26fc3401e98840333b
MGnuskina <mariya.gnuskina@gmail.com>
2016-01-11 13:24:03 +0200

    Changes to Ball proj

---
+++ SaveLoadTask/Ball/Ball/YAMLFiles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Yaml.Serialization;
+using System.IO;
+
+namespace Ball
+{
+    public class YAMLFiles : IWorkWithFiles
+    {
+        public List<BouncingBallClass> Load(string FilePath)
+        {
+            List<BouncingBallClass> balls = new List<BouncingBallClass>();
+            YamlSerializer reader = new YamlSerializer();
+            StreamReader file = new StreamReader(FilePath);
+            balls = (List<BouncingBallClass>)reader.Deserialize(file, typeof(List<BouncingBallClass>))[0];
+            return balls;
+        }
+
+        public void Save(List<BouncingBallClass> balls, string FilePath)
+        {
+            YamlSerializer save = new YamlSerializer();
+            FileStream file = File.Create(FilePath);
+            save.Serialize(file, balls);
+            file.Close();
+        }
+    }
+}
