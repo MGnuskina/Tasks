308f6d15b03b52047eb0fb258e2ec230f4f1144f
MGnuskina <mariya.gnuskina@gmail.com>
2016-01-20 13:52:52 +0200

    New project MVC Person

---
+++ Person MVC/Person/PersonCL/IPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonClassLibrary
+{
+    public interface IPersonRepository<T>
+    {
+        void Create(T item);
+        IEnumerable<T> GetAll();
+        T GetByID(int id);
+        void Update(T item);
+        void Delete(T item);
+    }
+}
